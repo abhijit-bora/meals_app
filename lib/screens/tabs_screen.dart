4f461ccbc61de687a4a58fd57064399c3e343927
abhijit-bora <50410295+abhijit-bora@users.noreply.github.com>
2021-10-31 23:55:44 +0530

    bottom tabs added

--- lib/screens/tabs_screen.dart
+++ lib/screens/tabs_screen.dart
@@ -8,6 +8,18 @@ class TabScreen extends StatefulWidget {
 }
 
 class _TabScreenState extends State<TabScreen> {
+  List<Widget> _pages = [
+    CategoriesScreen(),
+    FavoriteScreen(),
+  ];
+  int _seletPageIndex = 0;
+
+  void _selectPage(int index) {
+    setState(() {
+      _seletPageIndex = index;
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
     return DefaultTabController(
@@ -15,23 +27,23 @@ class _TabScreenState extends State<TabScreen> {
         child: Scaffold(
           appBar: AppBar(
             title: Text('Meals'),
-            bottom: TabBar(
-              tabs: <Widget>[
-                Tab(
-                  icon: Icon(Icons.category),
-                  text: 'Categories',
-                ),
-                Tab(
-                  icon: Icon(Icons.favorite),
-                  text: 'Favorites',
-                )
-              ],
-            ),
           ),
-          body: TabBarView(
-            children: <Widget>[
-              CategoriesScreen(),
-              FavoriteScreen(),
+          body: _pages[_seletPageIndex],
+          bottomNavigationBar: BottomNavigationBar(
+            onTap: _selectPage,
+            backgroundColor: Theme.of(context).primaryColor,
+            unselectedItemColor: Colors.white,
+            selectedItemColor: Theme.of(context).accentColor,
+            currentIndex: _seletPageIndex,
+            items: [
+              BottomNavigationBarItem(
+                icon: Icon(Icons.category),
+                title: Text('Categories'),
+              ),
+              BottomNavigationBarItem(
+                icon: Icon(Icons.favorite),
+                title: Text('Favorite'),
+              ),
             ],
           ),
         ));
